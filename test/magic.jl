60d6a1b0795851689c2f58756540e1e45a693f8d
MilesCranmer <miles.cranmer@gmail.com>
2023-02-10 18:13:17 -0500

    Create unit tests for Magic macro

---
+++ test/magic.jl
@@ -0,0 +1,71 @@
+@testset "Split + Join" begin
+
+  r = @Magic(w = [1, 5, 10]) do x
+    sum(w .* x)
+  end
+  @test Flux.params(r) == Flux.Params([[1, 5, 10]])
+  @test r([1, 1, 1]) == 1 + 5 + 10
+  @test r([1, 2, 3]) == 1 + 2 * 5 + 3 * 10
+  @test r(ones(3, 3)) == 3 * (1 + 5 + 10)
+
+  # Test gradients:
+  @test gradient(r, [1, 1, 1])[1] == [1, 5, 10]
+
+  d = @Magic(in = 5, out = 7, W = randn(out, in), b = zeros(out), act = relu) do x
+    y = W * x
+    act.(y .+ b)
+  end
+
+  @test size.(Flux.params(d)) == [(7, 5), (7,)]
+
+  @test size(d(ones(5, 10))) == (7, 10)
+  @test all(d(randn(5, 10)) .>= 0)
+
+  # Test gradients:
+  y, ∇ = Flux.withgradient(Flux.params(d)) do
+    input = randn(5, 32)
+    desired_output = randn(7, 32)
+    prediction = d(input)
+    sum((prediction - desired_output) .^ 2)
+  end
+  @test typeof(y) == Float64
+  grads = ∇.grads
+  @test typeof(grads) <: IdDict
+  @test length(grads) == 3
+  @test size.(values(grads)) == [(7, 5), (), (7,)]
+
+
+  # MLP:
+  n_in = 1
+  n_out = 1
+  nlayers = 3
+
+  model = @Magic(
+    w1 = Dense(n_in, 128),
+    w2 = [Dense(128, 128) for i = 1:nlayers],
+    w3 = Dense(128, n_out),
+    act = relu
+  ) do x
+    embed = act(w1(x))
+    for w in w2
+      embed = act(w(embed))
+    end
+    out = w3(embed)
+    return out
+  end
+
+  @test size.(Flux.params(model)) == [
+    (128, 1),
+    (128,),
+    (128, 128),
+    (128,),
+    (128, 128),
+    (128,),
+    (128, 128),
+    (128,),
+    (1, 128),
+    (1,),
+  ]
+  @test size(model(randn(n_in, 32))) == (1, 32)
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Flux, Fluxperimental
 
 @testset "Fluxperimental.jl" begin
   include("split_join.jl")
+  include("magic.jl")
 end
