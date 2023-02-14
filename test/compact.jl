e84f1e3e512b76c87e3236be1eef9772f0f785a9
MilesCranmer <miles.cranmer@gmail.com>
2023-02-14 15:56:27 -0500

    Update test for `_big_show` printing

--- test/compact.jl
+++ test/compact.jl
@@ -1,5 +1,23 @@
 import Fluxperimental: @compact
 
+# Strip both strings of spaces, and then test:
+function similar_strings(s1, s2)
+  s1 = replace(s1, r"\s" => "")
+  s2 = replace(s2, r"\s" => "")
+  # Display any differences:
+  if s1 != s2
+    println(stderr, "s1: ", s1)
+    println(stderr, "s2: ", s2)
+  end
+  return s1 == s2
+end
+
+function get_model_string(model)
+  io = IOBuffer()
+  show(io, MIME"text/plain"(), model)
+  String(take!(io))
+end
+
 @testset "@compact" begin
 
   r = @compact(w = [1, 5, 10]) do x
@@ -71,20 +89,45 @@ import Fluxperimental: @compact
   @test size(model(randn(n_in, 32))) == (1, 32)
 
   # Test string representations:
-  model = @compact(w=randn(32, 32)) do x, y
-    tmp = sum(w .* x)
+  model = @compact(w=Dense(32 => 32)) do x, y
+    tmp = sum(w(x))
     return tmp + y
   end
-  @test string(model) == """@compact(
-  w = randn(32, 32),
-) do x, y
-    tmp = sum(w .* x)
+  expected_string = """@compact(
+    w = Dense(32=>32), #1_056 parameters
+  ) do x, y
+    tmp = sum(w(x))
     return tmp + y
-end"""
-  model = @compact(w=randn(32, 32), name="Linear(...)") do x, y
-    tmp = sum(w .* x)
+  end"""
+  @test similar_strings(get_model_string(model), expected_string)
+
+  model = @compact(w=Dense(32, 32), name="Linear") do x, y
+    tmp = sum(w(x))
     return tmp + y
   end
-  @test string(model) == "Linear(...)"
+  expected_string = "Linear(
+    w = Dense(32=>32), #1_056 parameters
+  )"
+  @test similar_strings(get_model_string(model), expected_string)
+
+  # Hierarchical models should work too:
+  model1 = @compact(w1=Dense(32=>32, relu), w2=Dense(32=>32, relu)) do x
+    w2(w1(x))
+  end
+  model2 = @compact(w1=model1, w2=Dense(32=>32, relu)) do x
+    w2(w1(x))
+  end
+  expected_string = """@compact(
+    w1 = @compact(
+      w1 = Dense(32 => 32, relu),         # 1_056 parameters
+      w2 = Dense(32 => 32, relu),         # 1_056 parameters
+    ) do x
+      w2(w1(x))
+  end,
+    w2 = Dense(32 => 32, relu),           # 1_056 parameters
+  ) do x
+      w2(w1(x))
+  end                  # Total: 6 arrays, 3_168 parameters, 13.047 KiB."""
+  @test similar_strings(get_model_string(model2), expected_string)
 
 end
