3788db87fac630f7297f96bc492a3c1ee93d0d24
Matthew Schlegel <mkschleg@gmail.com>
2023-03-28 15:59:42 -0600

    Added recur, fixed some chain stuff, added broken recur tests.

--- src/chain.jl
+++ src/chain.jl
@@ -12,8 +12,10 @@ function apply(chain::Flux.Chain, x)
   Flux.Chain(layers), out
 end
 
+apply(layer, x) = _apply_to_layer(layer, x)
+
 function _apply(layers::NamedTuple{NMS, TPS}, x) where {NMS, TPS}
-  layers, out = _apply_to_layer(Tuple(layers), x)
+  layers, out = _apply(Tuple(layers), x)
   NamedTuple{NMS}(layers), out
 end
 
@@ -29,7 +31,7 @@ end
 # example pulled from https://github.com/mcabbott/Flux.jl/blob/chain_rrule/src/cuda/cuda.jl
 function ChainRulesCore.rrule(cfg::ChainRulesCore.RuleConfig, ::typeof(_scan), layers, x)
   duo = accumulate(layers; init=((nothing, x), nothing)) do ((pl,  input), _), cur_layer
-    out, back = ChainRulesCore.rrule_via_ad(cfg, _apply, cur_layer, input)
+    out, back = ChainRulesCore.rrule_via_ad(cfg, _apply_to_layer, cur_layer, input)
   end
   outs = map(first, duo)
   backs = map(last, duo)
@@ -61,81 +63,3 @@ end
 
 _apply_to_layer(layer::Flux.Chain, x) = apply(layer, x)
 _apply_to_layer(layer, x) = layer, layer(x)
-
-
-"""
-  NM_Recur
-
-Non-mutating Recur. An experimental recur interface for the new chain api.
-"""
-struct NM_Recur{T,S}
-  cell::T
-  state::S
-end
-
-function _apply_to_layer(m::NM_Recur, x)
-  state, y = m.cell(m.state, x)
-  return NM_Recur(m.cell, state), y
-end
-
-# This is the same hacky way we do it from Flux.Recur
-function _apply_to_layer(m::NM_Recur, x::AbstractArray{T, 3}) where T
-  # h = [m(x_t) for x_t in eachlastdim(x)]
-  l, h = _apply_to_layer(m, Flux.eachlastdim(x))
-  sze = size(h[1])
-  l, reshape(reduce(hcat, h), sze[1], sze[2], length(h))
-end
-
-Flux.@functor NM_Recur
-Flux.trainable(a::NM_Recur) = (; cell = a.cell)
-
-Base.show(io::IO, m::NM_Recur) = print(io, "Recur(", m.cell, ")")
-
-NM_RNN(a...; ka...) = NM_Recur(Flux.RNNCell(a...; ka...))
-NM_Recur(m::Flux.RNNCell) = NM_Recur(m, m.state0)
-
-##
-# Apply timeseries data in vectors to chains
-##
-
-"""
-  _dscan
-
-This method does a scan over the data. The rrule is note implemented yet, but should be similar to _scan above.
-"""
-function _dscan(layer, xs)
-  ret = accumulate(xs; init=(layer, nothing)) do (l, y), x
-    _apply_to_layer(l, x)
-  end
-  ret[end][1], map(last, ret)
-end
-
-# rrule for _dscan
-function ChainRulesCore.rrule(cfg::ChainRulesCore.RuleConfig, ::typeof(_dscan), layer, xs)
-  # Not implemented yet.
-  function _dscan_pullback(dy)
-    throw("Pullback for _dscan not implemented yet")
-  end
-  return _dscan(layer, xs), _dscan_pullback
-end
-
-
-# Specific details for working with time-series data.
-function apply_timeseries_toplevel(chain::Flux.Chain, xs)
-  _dscan(chain, xs)
-end
-
-# To apply the timeseries data at the layer levels, we can re-use the applies above.
-# We only need to add a default method to _apply_to_layer, and then a method for
-# stateful _apply_to_layer using the _dscan above.
-function apply_timeseries_layerlevel(chain::Flux.Chain, xs)
-  apply(chain, xs)
-end
-
-function _apply_to_layer(l, xs::AbstractVector{<:AbstractArray})
-  l, [l(x) for x in xs]
-end
-
-function _apply_to_layer(l::NM_Recur, xs::Union{AbstractVector{<:AbstractArray}, Base.Generator})
-  _dscan(l, xs)
-end
