72a7fe149b4403b605e3897ee3a750b20389804b
Matthew Schlegel <mkschleg@gmail.com>
2023-08-03 14:55:10 -0600

    Fixed tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,11 @@ using Test
 using Flux, Fluxperimental
 
 @testset "Fluxperimental.jl" begin
-  # include("split_join.jl")
+  include("split_join.jl")
 
-  # include("chain.jl")
+  include("chain.jl")
 
-  # include("compact.jl")
+  include("compact.jl")
 
   include("new_recur.jl")
 
