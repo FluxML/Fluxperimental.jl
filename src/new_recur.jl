7b60350bc843ee0f4bd22582fb18e088d1c1a4f1
Matthew Schlegel <mkschleg@gmail.com>
2023-08-03 10:25:59 -0600

    Fixed gradients using Lux's impl.

--- src/new_recur.jl
+++ src/new_recur.jl
@@ -1,5 +1,6 @@
 
-
+import Flux: ChainRulesCore
+# import ChainRulesCore: rrule, HasReverseMode
 
 ##### Helper scan funtion which can likely be put into NNLib. #####
 """
@@ -8,27 +9,129 @@
 Recreating jax.lax.scan functionality in julia. Takes a function, initial carry and a sequence,
 then returns the output sequence and the final carry. 
 """
+
 function scan_full(func, init_carry, xs::AbstractVector{<:AbstractArray})
-  # get the first input to setup the initial state,
-  # get the rest of the input to run the fold over.
-  x_init, x_rest = Iterators.peel(xs)
-  # the following does the same as peel, but doesn't produce correct gradients?
-  ### x_init = first(xs)
-  ### x_rest = xs[begin+1:end]
-
-  # set up the initial state of the fold.
-  (carry_, out_) = func(init_carry, x_init)
-  init = (carry_, [out_])
-
-  # recurrence operation used in the fold. Takes the state  of the
-  # folde and the next input, returns the new state.
-  function __recurrence_op((carry, outputs), input)
-    carry, out = func(carry, input)
-    return carry, vcat(outputs, [out])
-  end
-  # Fold left to right.
-  foldl(__recurrence_op, x_rest; init)
+   # Recurrence operation used in the fold. Takes the state of the
+   # fold and the next input, returns the new state.
+   function recurrence_op((carry, outputs), input)
+       carry, out = func(carry, input)
+       return carry, vcat(outputs, [out])
+   end
+   # Fold left to right.
+   return Base.mapfoldl_impl(identity, recurrence_op, (init_carry, empty(xs)), xs)
 end
+
+function ChainRulesCore.rrule(
+  config::ChainRulesCore.RuleConfig{>:ChainRulesCore.HasReverseMode},
+  ::typeof(Base.mapfoldl_impl),
+  ::typeof(identity),
+  op::G,
+  init,
+  x::Union{AbstractArray, Tuple};
+) where {G}
+  hobbits = Vector{Any}(undef, length(x))  # Unfornately Zygote needs this
+  accumulate!(hobbits, x; init=(init, nothing)) do (a, _), b
+  # hobbits = accumulate(x; init=(init, nothing)) do (a, _), b
+        c, back = ChainRulesCore.rrule_via_ad(config, op, a, b)
+    end
+    y = first(last(hobbits))
+    axe = axes(x)
+    project = ChainRulesCore.ProjectTo(x)
+    function unfoldl(dy)
+        trio = accumulate(Iterators.reverse(hobbits); init=(0, dy, 0)) do (_, dc, _), (_, back)
+            ds, da, db = back(dc)
+        end
+        dop = sum(first, trio)
+        dx = map(last, Iterators.reverse(trio))
+        d_init = trio[end][2]
+        return (ChainRulesCore.NoTangent(), ChainRulesCore.NoTangent(), dop, d_init, project(reshape(dx, axe)))
+    end
+    return y, unfoldl
+end
+
+# function CRC.rrule(cfg::RuleConfig{>:HasReverseMode},
+#     ::typeof(Base.mapfoldl_impl),
+#     op::G,
+#     x::AbstractArray,
+#     init) where {G}
+#     list, start = x, init
+#     hobbits = Vector{Any}(undef, length(list))  # Unfornately Zygote needs this
+#     accumulate!(hobbits, list; init=(start, nothing)) do (a, _), b
+#         return CRC.rrule_via_ad(cfg, op, a, b)
+#     end
+#     y = first(last(hobbits))
+#     ax = axes(x)
+#     project = ChainRulesCore.ProjectTo(x)
+#     function ∇mapfoldl_impl(Δ)
+#         trio = accumulate(Iterators.reverse(hobbits); init=(0, Δ, 0)) do (_, dc, _), (_, back)
+#             return back(dc)
+#         end
+#         ∂op = sum(first, trio)
+#         ∂x = map(last, Iterators.reverse(trio))
+#         return (ChainRulesCore.NoTangent(), ChainRulesCore.NoTangent(), dop, d_init, project(reshape(dx, axe))) #NoTangent(), ∂op, project(reshape(∂x, ax)), trio[end][2]
+#     end
+#     return y, ∇mapfoldl_impl
+# end
+
+
+
+# function scan_full(func, init_carry, xs::AbstractVector{<:AbstractArray})
+#   function __recurrence_op(::Tuple{Nothing, Nothing}, input)
+#     carry, out = func(init_carry, input)
+#     return carry, [out]
+#   end
+
+#   # recurrence operation used in the fold. Takes the state  of the
+#   function __recurrence_op((carry, outputs), input)
+#     carry, out = func(carry, input)
+#     return carry, vcat(outputs, [out])
+#   end
+
+#   # Fold left to right.
+#   foldl(__recurrence_op, xs; init=(nothing, nothing))
+# end
+
+# _cat_output(::Nothing, out) = [out]
+# _cat_output(outputs, out) = vcat(outputs, [out])
+
+# function scan_full(func, init_carry, xs::AbstractVector{<:AbstractArray})
+#   # Recurrence operation used in the fold. Takes the state of the
+#   # fold and the next input, returns the new state.
+#   function recurrence_op((carry, outputs), input)
+#     carry = ifelse(carry === nothing, init_carry, carry)
+#     carry, out = func(carry, input)
+#     return carry, _cat_output(outputs, out)
+#   end
+#   # Fold left to right.
+#   return foldl(recurrence_op, xs; init=(nothing, nothing))
+# end
+
+# function scan_full(func, init_carry, xs::AbstractVector{<:AbstractArray})
+#   # get the first input to setup the initial state,
+#   # get the rest of the input to run the fold over.
+#   # x_init, x_rest = Iterators.peel(xs)
+#   # the following does the same as peel, but doesn't produce correct gradients?
+#   ### x_init = first(xs)
+#   ### x_rest = xs[begin+1:end]
+
+#   # set up the initial state of the fold.
+#   # (carry_, out_) = func(init_carry, x_init)
+#   # init = (carry_, [out_])
+#   function __recurrence_op(::Nothing, input)
+#     carry, out = func(init_carry, input)
+#     return carry, [out]
+#   end
+
+#   # recurrence operation used in the fold. Takes the state  of the
+#   # folde and the next input, returns the new state.
+#   function __recurrence_op((carry, outputs), input)
+#     carry, out = func(carry, input)
+#     return carry, vcat(outputs, [out])
+#   end
+#   # Fold left to right.
+#   # foldl(__recurrence_op, xs; init=nothing)
+#   foldl_init(__recurrence_op, xs)
+# end
 
 function scan_full(func, init_carry, x_block)
   # x_block is an abstractarray and we want to scan over the last dimension.
