305b8b3570d9dd41bdd7e28856d743bb009f2d24
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2023-10-14 18:36:54 -0400

    tweaks

--- test/compact.jl
+++ test/compact.jl
@@ -19,11 +19,7 @@ function similar_strings(s1, s2)
   return s1 == s2
 end
 
-function get_model_string(model)
-  io = IOBuffer()
-  show(io, MIME"text/plain"(), model)
-  String(take!(io))
-end
+get_model_string(model) = repr(MIME("text/plain"), model)
 
 @testset "@compact" begin
 
@@ -142,7 +138,8 @@ end
 #=  # This test is broken:
 
 julia> model1 = @compact(w1=Dense(32=>32, relu), w2=Dense(32=>32, relu)) do x
-             w2(w1(x));
+             w2(w1(x))
+            end;
 
 julia> model2 = @compact(w1=model1, w2=Dense(32=>32, relu)) do x
              w2(w1(x))
