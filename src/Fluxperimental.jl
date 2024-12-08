51b1992bd5374dd8e8fe254ea2c0f6cd8c95973f
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2024-12-08 16:18:41 -0500

    take II

--- src/Fluxperimental.jl
+++ src/Fluxperimental.jl
@@ -2,27 +2,27 @@ module Fluxperimental
 
 using Flux
 
-include("split_join.jl")
-export Split, Join
+# include("split_join.jl")  # crashes because of https://github.com/FluxML/Flux.jl/issues/2545
+# export Split, Join
 
 include("train.jl")
 export shinkansen!
 
 include("reactant.jl")
-export Fluxactor
+export Reactor
 
 include("chain.jl")
 
-include("compact.jl")
-export @compact
+# include("compact.jl")
+# export @compact
 
-include("noshow.jl")
-export NoShow
+# include("noshow.jl")
+# export NoShow
 
 include("autostruct.jl")
 export @autostruct
 
-include("new_recur.jl")
+# include("new_recur.jl")
 
 include("mooncake.jl")
 export Moonduo
